a611b587c858251eed2d81a497edec7488e49b28
jblackmor <jasonblackmor@gmail.com>
2025-02-14 00:39:58 -0600

    creating circleshape & player sprite, adding player rotation

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
+
         screen.fill(color='black')
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
