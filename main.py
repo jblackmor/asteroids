25e6f63ee08654c967a855b3e36e6bae4127145e
jblackmor <jasonblackmor@gmail.com>
2025-02-17 03:46:05 -0600

    finishing up base game, adding asteroids, bullets, shot timer, destruction, and asteroid splitting

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Asteroids')
+
     clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()  # everything that can be updated (inc screen)
     drawable = pygame.sprite.Group()  # everything that can be drawn (inc player, asteroids, shots)
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)  # add all Player instances to these groups
+    Shot.containers = (updatable, drawable, shots)
     Asteroid.containers = (updatable, drawable, asteroids)  # add all Asteroid instances to these groups
     AsteroidField.containers = updatable
 
@@ -40,6 +45,10 @@ def main():
             if a.check_for_collisions(player):
                 print('Game over!')
                 sys.exit()
+            for s in shots:
+                if s.check_for_collisions(a):
+                    s.kill()
+                    a.split()
 
         screen.fill(color='black')
         
